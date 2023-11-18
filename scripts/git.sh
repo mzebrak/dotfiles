15b7c25b9019a22352cf2ba85d9ef4c78fb520f3
Mateusz Żebrak <mzebrak@syncad.com>
2023-11-18 11:25:08 +0100

    Add delta installation

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 # shellcheck source=../scripts/util.sh
 source "$(pwd)/scripts/util.sh"
 
+DELTA_VERSION="${DELTA_VERSION:=0.16.5}"
+
 do_configure() {
   info "[git][configure] Creating config file symlink started..."
   ln -fs "$(pwd)/git/gitconfig" "${HOME}/.gitconfig"
@@ -13,6 +15,14 @@ do_configure() {
   info "[git][configure] Creating a commit-template file started..."
   touch "$(pwd)/git/commit-template"
   success "[git][configure] Creating a commit-template file done"
+
+
+  info "[git][configure][delta] Delta installation started..."
+  local delta=/tmp/delta.deb
+  download "https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/git-delta_${DELTA_VERSION}_amd64.deb" "${delta}"
+  sudo dpkg -i "${delta}"
+  success "[git][configre][delta] Installation done"
+
 }
 
 main() {
